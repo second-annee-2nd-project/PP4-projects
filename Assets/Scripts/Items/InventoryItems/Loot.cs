256df8addb2a57f0277da6b5cf797156971cead7
Khuzast <67412119+Khuzast@users.noreply.github.com>
2021-07-08 16:33:33 +0200

    Script Loot

---
+++ Assets/Scripts/Items/InventoryItems/Loot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    private int amountToLoot;
+
+    public int AmountToLoot
+    {
+        get => amountToLoot;
+        set => amountToLoot = value;
+    }
+
+    public void Looted(PlayerBehaviour playerBehaviour)
+    {
+        playerBehaviour.Loot(amountToLoot);
+        Destroy(gameObject);
+    }
+}
