459cd1639a4ffd962a64cb415d1b8b158c727e31
DESKTOP-SA1IRM2\Fares <6-15352@saeinstitute.edu>
2021-07-28 16:31:16 +0200

    ajout son et particules

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+   private AudioSource audioSource;
+    public AudioSource AudioSource
+    {
+        get => audioSource;
+        set => audioSource = value;
+    }
+}
